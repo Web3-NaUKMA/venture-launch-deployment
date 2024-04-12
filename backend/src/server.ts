0daa2560fa6252749ad4b4dbf1ab51da12a7bc5e
root <root@hqua0320566.online-vm.com>
2024-04-12 15:34:45 +0300

    Updated configuration files

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,7 +34,7 @@ app.use(
 
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_HOST}:${process.env.FRONTEND_PORT}`,
+    origin: `${process.env.FRONTEND_HOST}`,
     credentials: true,
   }),
 );
