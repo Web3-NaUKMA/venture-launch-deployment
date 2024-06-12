f62e69c5395414981c45ae2cdda4031636a2aa58
kalyonekenobe <igumssukr@gmail.com>
2024-06-12 14:58:54 +0300

    Fixed request utils errors

--- frontend/src/utils/request.utils.ts
+++ frontend/src/utils/request.utils.ts
@@ -1,6 +1,6 @@
-import { IRequestQueryParams } from '../types/app.types';
+import { RequestQueryParams } from '../types/app.types';
 
-export const serializeQueryParams = (queryParams: IRequestQueryParams): string => {
+export const serializeQueryParams = (queryParams: RequestQueryParams): string => {
   return Object.entries(queryParams)
     .map(([key, value]) => {
       if (Array.isArray(value)) return `${key}=${value.join(',')}`;
